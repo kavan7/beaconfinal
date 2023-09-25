5700238862d6897a85a1b550caef00e4870328d1
gorgocaptain <gorgocaptain@gmail.com>
2023-09-25 10:05:16 -0400

    moved backspace and enter up in beardle

--- src/components/Beardle.jsx
+++ src/components/Beardle.jsx
@@ -24,8 +24,8 @@ function Beardle() {
   const keyboardLayout = [
     ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
     ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
-    ['Z', 'X', 'C', 'V', 'B', 'N', 'M'],
-    ['-', '>'], // Add Backspace and Enter keys
+    ['-','Z', 'X', 'C', 'V', 'B', 'N', 'M', '>'],
+    
   ];
   
 
