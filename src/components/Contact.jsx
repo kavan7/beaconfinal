09b655ecffaadcf3d4aad6d957c6cd84e4953f14
kavan7 <abeyratnekavan@gmail.com>
2023-09-26 13:41:20 -0400

    contact page added and functioning

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,8 @@ import {motion} from 'framer-motion';
 import emailjs from '@emailjs/browser'
 import {styles} from '../styles';
 import { slideIn } from '../utils/motion';
+
+
 const Contact = () => {
   const formRef = useRef();
     const [form, setForm] = useState({
@@ -13,20 +15,119 @@ const Contact = () => {
 
   });
   const [loading, setLoading] = useState(false);
-  const handleChange = (e) => {}
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = target;
 
-  const handleSubmit = (e) => {}
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs.send(
+      'service_c37uz7i', 'template_q567k5e',
+    {
+      from_name: form.name,
+      to_name: 'Beacon',
+      from_email: form.email,
+      to_email: 'contactmcibeacon@gmail.com',
+      message: form.message,
+
+    },
+    'TatXTYiVC966_Fzwt'
+    )
+    .then(() =>{
+      setLoading(false);
+      alert('Thank you. We will get back to you as soon as possible.')
+      
+      setForm({
+        name: '',
+        email: '',
+        message: '',
+
+      })
+
+    }, (error) => {
+      setLoading(false)
+      console.log(error)
+
+      alert('Something went wrong.')
+    })
+  }
   return (
-    <div className='xl:mt-12 xl:flex-row
-    flex-col-reverse flex gap-10 overlow-hidden'>
+    <div className='xl:mt-8 xl:flex-row
+    flex-col-reverse flex  overlow-hidden'>
       <motion.div
       
       variants={slideIn('left', "tween", 0.2, 1)}
-      className='flex-[0.75] article-card-container p-8 rounded-2xl'>
-          <p>Send a letter to the editors!</p>
-          <h3 className={styles.sectionHeadText} >Contact</h3>
-      </motion.div>
+      className='flex-[0.75] w-[30%] contact-card-container p-8 rounded-2xl'>
+          <p className='text-gray-500'>Send a message to the editors!</p>
+          <h3 className={styles.sectionHeadText} >Contact.</h3>
 
+          <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className='mt-0 flex flex-col gap-8'>
+            <label className='flex flex-col'>
+              <span className='text-gray-900 ml-[10px] mb-[10px] font-semibold underline-offset-1'>
+                Your Name
+              </span>
+              <input
+              type='text'
+              name='name'
+              value={form.name}
+              onChange={handleChange}
+              placeholder="What's your name"
+              className='bg-gray-100 py-4 px-6 placeholder:text-gray-500 focus:outline-none text-gray-900 rounded-lg outlined-none border-none font-medium'>
+
+              </input>
+
+            </label>
+            <label className='flex flex-col'>
+              <span className='text-gray-900 ml-[10px] mb-[10px] font-semibold underline-offset-1'>
+                Your Email
+              </span>
+              <input
+              type='email'
+              name='email'
+              value={form.email}
+              placeholder="What's your email"
+              onChange={handleChange}
+              className='bg-gray-100 py-4 px-6 placeholder:text-gray-500 focus:outline-none text-gray-900 rounded-lg outlined-none border-none font-medium'>
+
+              </input>
+
+            </label>
+            <label className='flex flex-col'>
+              <span className='text-gray-900 ml-[10px] mb-[10px] font-semibold underline-offset-1'>
+               Message
+              </span>
+              <textarea
+              rows="7"
+              
+              name='message'
+              value={form.message}
+              placeholder="What would you like to say"
+              onChange={handleChange}
+              className='bg-gray-100 py-4 px-6 placeholder:text-gray-500 focus:outline-none text-gray-900 rounded-lg outlined-none border-none font-medium'>
+
+              </textarea>
+
+            </label>
+            <button
+            type='submit'
+            className=' mt-[-23px] justify-right  bg-gray-300 py-3 px-8 outline-none w-fit text-gray-700 font-bold shadow-md shadow-primary rounded-xl'>
+              {loading ? 'Sending...' : 'Send'}
+            </button>
+
+          </form>
+      </motion.div>
+  
     </div>
   )
 }
