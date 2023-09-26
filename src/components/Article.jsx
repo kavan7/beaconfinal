e81642de366ca2995be93fc76d582f876c88ca09
gorgocaptain <gorgocaptain@gmail.com>
2023-09-26 19:29:40 -0400

    article changes 3

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -5,10 +5,11 @@ const Article = ({ article }) => {
   return (
     <div>
       <h2 className='title'>{article.title}</h2>
-      <p className='author'>Author: {article.author}</p>
-      <p className='date'>Date: {article.date}</p>
+      
+      
       <img src={article.image} className='article-image-page'>
       </img>
+      <p className='author'>Author: {article.author}<p className='date'>Date: {article.date}</p></p>
       <p className='content'>{article.content}</p>
       <div dangerouslySetInnerHTML={{ __html: article.content }} />
     </div>
