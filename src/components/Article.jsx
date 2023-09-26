938db8e7668b5b5f3ed00192df7289bf088b04d7
gorgocaptain <gorgocaptain@gmail.com>
2023-09-26 14:49:59 -0400

    article changes

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import articles from '../constants/Articles';
+import "./article.css";
 const Article = ({ article }) => {
   return (
     <div>
-      <h2>{article.title}</h2>
-      <p>Author: {article.author}</p>
-      <p>Date: {article.date}</p>
-      <p>{article.preview}</p>
+      <h2 className='title'>{article.title}</h2>
+      <p className='author'>Author: {article.author}</p>
+      <p className='date'>Date: {article.date}</p>
+      <p className='content'>{article.content}</p>
       <div dangerouslySetInnerHTML={{ __html: article.content }} />
     </div>
   );
