800896f22f3515e0403bcca94e584e8b1f8100c3
gorgocaptain <gorgocaptain@gmail.com>
2023-09-26 20:51:03 -0400

    members page

--- src/components/Members.jsx
+++ src/components/Members.jsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import React from 'react';
+import {
+  editorsInChief,
+  managingEditor,
+  copyEditors,
+  layoutHead,
+  comicsIllustrationsHead,
+  photographyHead,
+  webTeamHead,
+  secretary,
+  publicityHead,
+  columnist,
+} from "../constants/Members";
+import './Members.css'; // Import the CSS file
 
 const Members = () => {
   return (
-    <div>Members</div>
-  )
+    <div className="members-container">
+      <Category title="Editors-in-Chief" members={editorsInChief} />
+      <Category title="Managing Editor" members={managingEditor} />
+      <Category title="Copy Editors" members={copyEditors} />
+      <Category title="Layout Head" members={layoutHead} />
+      <Category title="Comics/Illustrations Head" members={comicsIllustrationsHead} />
+      <Category title="Photography Head" members={photographyHead} />
+      <Category title="Web Team Head" members={webTeamHead} />
+      <Category title="Secretary" members={secretary} />
+      <Category title="Publicity Head" members={publicityHead} />
+      <Category title="Columnist" members={columnist} />
+      {/* Add more categories here */}
+    </div>
+  );
 }
 
-export default Members
+const Category = ({ title, members }) => (
+  <div className="category-container">
+    <h2 className="category-title">{title}</h2>
+    <ul className="member-list">
+      {members.map((member, index) => (
+        <li key={index} className="member-item">
+          <div className="member-info">
+            <img src={member.pfp} alt={member.name} className="member-image" />
+            <h3 className="member-name">{member.name}</h3>
+          </div>
+          <a
+            href={member.iglink}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="member-link"
+          >
+            Instagram
+          </a>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
+
+export default Members;
--- src/constants/Members.js
+++ src/constants/Members.js
@@ -1,3 +1,4 @@
+
 import {aidanSchreder,
     alishaWong,
     anneliAloeCuffe,
@@ -39,22 +40,155 @@ import {aidanSchreder,
     susannaLiao,
     tessaWilliams,
     theoBoychev
-  } from '../assets';
+  } from '../assets/images/avatars';
 
 
-const chiefs= [{
-    name: "Skyler Xiang",
-    badgebg: '#ff5733',
-    pfp: skylerXiang,
-    iglink: 'https://www.instagram.com/skchuylear/'
-},
-{
-    name: "Rababb Pannu",
-    badgebg: '#ff5733',
-    pfp: rababbPannu,
-    iglink: 'https://www.instagram.com/rababb_p/'
-},
-]
 
-
-export default chiefs;
+  export const editorsInChief = [
+    {
+      name: "Skyler Xiang",
+      badgebg: '#ff5733',
+      pfp: skylerXiang,
+      iglink: 'https://www.instagram.com/skchuylear/'
+    },
+    {
+      name: "Rababb Pannu",
+      badgebg: '#ff5733',
+      pfp: rababbPannu,
+      iglink: 'https://www.instagram.com/rababb_p/'
+    }
+    // Add more Editors-in-Chief as needed
+  ];
+  
+  export const managingEditor = [
+    {
+      name: "Madeline Li",
+      badgebg: '#ff5733',
+      pfp: defaultImage,
+      iglink: 'https://www.instagram.com/madelineli/'
+    }
+    // Add more Managing Editors as needed
+  ];
+  
+  export const copyEditors = [
+    {
+      name: "Amisha Ramiah",
+      badgebg: '#ff5733',
+      pfp: defaultImage,
+      iglink: 'https://www.instagram.com/amisharamiah/'
+    },
+    {
+      name: "Ryan Gordon",
+      badgebg: '#ff5733',
+      pfp: ryanGordon,
+      iglink: 'https://www.instagram.com/ryangordon/'
+    },
+    {
+      name: "Julia Beaman",
+      badgebg: '#ff5733',
+      pfp: juliaBeaman,
+      iglink: 'https://www.instagram.com/juliabeaman/'
+    }
+    // Add more Copy Editors as needed
+  ];
+  
+  export const layoutHead = [
+    {
+      name: "Eric Wu",
+      badgebg: '#ff5733',
+      pfp: defaultImage,
+      iglink: 'https://www.instagram.com/ericwu/'
+    }
+    // Add more Layout Heads as needed
+  ];
+  
+  export const comicsIllustrationsHead = [
+    {
+      name: "Ruchi Tilva",
+      badgebg: '#ff5733',
+      pfp: defaultImage,
+      iglink: 'https://www.instagram.com/ruchitilva/'
+    }
+    // Add more Comics/Illustrations Heads as needed
+  ];
+  
+  export const photographyHead = [
+    {
+      name: "Christine Lee",
+      badgebg: '#ff5733',
+      pfp: christineLee,
+      iglink: 'https://www.instagram.com/christinelee/'
+    }
+    // Add more Photography Heads as needed
+  ];
+  
+  export const webTeamHead = [
+    {
+      name: "Aidan Schreder",
+      badgebg: '#ff5733',
+      pfp: aidanSchreder,
+      iglink: 'https://www.instagram.com/aidanschreder/'
+    },
+    {
+      name: "Nathan Perlman",
+      badgebg: '#ff5733',
+      pfp: nathanPerlman,
+      iglink: 'https://www.instagram.com/nathanperlman/'
+    }
+    // Add more Web Team Heads as needed
+  ];
+  
+  export const secretary = [
+    {
+      name: "Kavya Vinod",
+      badgebg: '#ff5733',
+      pfp: defaultImage,
+      iglink: 'https://www.instagram.com/kavyavinod/'
+    },
+    {
+      name: "Nathan Perlman",
+      badgebg: '#ff5733',
+      pfp: nathanPerlman,
+      iglink: 'https://www.instagram.com/nathanperlman/'
+    }
+    // Add more Secretaries as needed
+  ];
+  
+  export  const publicityHead = [
+    {
+      name: "Ellie Jiang",
+      badgebg: '#ff5733',
+      pfp: ellieJiang,
+      iglink: 'https://www.instagram.com/elliejiang/'
+    }
+    // Add more Publicity Heads as needed
+  ];
+  
+ export const columnist = [
+    {
+      name: "Nathan Tsui",
+      badgebg: '#ff5733',
+      pfp: defaultImage,
+      iglink: 'https://www.instagram.com/nathantsui/'
+    },
+    {
+      name: "Lukas Cousins",
+      badgebg: '#ff5733',
+      pfp: lukasCousins,
+      iglink: 'https://www.instagram.com/lukascousins/'
+    },
+    {
+      name: "Max Hutton",
+      badgebg: '#ff5733',
+      pfp: maxHutton,
+      iglink: 'https://www.instagram.com/maxhutton/'
+    },
+    {
+      name: "Aleksa Misic",
+      badgebg: '#ff5733',
+      pfp: defaultImage,
+      iglink: 'https://www.instagram.com/aleksamisic/'
+    }
+    // Add more Columnists as needed
+  ];
+  
