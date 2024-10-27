1aeda66a2cbbca3b88460871213ccc585bf507fd
Rababb Pannu <rababb.s.pannu@gmail.com>
2024-10-27 00:01:46 -0400

    vite config pls work

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginMd from 'vite-plugin-md'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.md', '**/*.JPG']
+  plugins: [react(), vitePluginMd()],
+  assetsInclude: ['/.md', '/.JPG'],
+  base: '/beaconfinal/',
 })
