1e6b5c100b07f9d259f36e8bd4e8e6586b2b2d58
Kavan Abeyratne <121369066+kavan7@users.noreply.github.com>
2024-10-27 00:06:32 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import vitePluginMd from 'vite-plugin-md'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vitePluginMd()],
   assetsInclude: ['/*.md', '/*.JPG'],
-  base: '/',
+  base: '/beaconfinal/',
 })
